b4291f38a4196b4d1c599029c3a3def6a32834de
Ta-Kakehi <takuya.9897@gmail.com>
2020-01-22 08:37:49 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(admin_name: "admin1",
+             password: "admin1",
+             is_admin: true
+)
