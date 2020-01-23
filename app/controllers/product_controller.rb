61d086590eff46bb04f9187076092ef6de9617eb
Ta-Kakehi <takuya.9897@gmail.com>
2020-01-23 04:46:30 +0000

    add id

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -94,9 +94,9 @@ class ProductController < ApplicationController
   
   def search
     if params[:search_name] 
-      @product = Product.where("product_name LIKE ?", "%#{params[:search_name]}%").group(:product_name,:price).order(id: "ASC")
+      @product = Product.where("product_name LIKE ?", "%#{params[:search_name]}%").group(:id,:product_name,:price).order(id: "ASC")
     else
-      @product = Product.where("category LIKE ?", "%#{params[:search_name]}%").group(:product_name,:price).order(id: "ASC")
+      @product = Product.where("category LIKE ?", "%#{params[:search_name]}%").group(:id,:product_name,:price).order(id: "ASC")
     end
     render("product/all_index")
   end 
